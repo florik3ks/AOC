ca3fa108e1dbde6bfd37e8799882f076bc071a0c
flori <floritrs@gmail.com>
2025-12-10 22:12:18 +0100

    d10p1

---
+++ 2025/src/bin/d10.rs
@@ -0,0 +1,146 @@
+use std::collections::{HashSet, VecDeque};
+use std::time::Instant;
+use std::{fs::File, io::Read};
+
+use regex::Regex;
+
+fn main() {
+    // get day number from file
+    let day = file!().split('/').last().unwrap()[1..3].to_owned();
+
+    // read input
+    let path = format!("input/input{day}.txt");
+    println!("{}", path);
+    let mut file = File::open(path).expect("File not found");
+    let mut input = String::new();
+    file.read_to_string(&mut input).expect("cannot read file");
+
+    // measure time needed to run both parts
+    let mut now: Instant;
+    let mut elapsed: std::time::Duration;
+
+    now = Instant::now();
+    let p1result = p1(&input);
+    elapsed = now.elapsed();
+
+    println!("Part 1 solution in: {:.2?}", elapsed);
+    println!("{}", p1result);
+
+    now = Instant::now();
+    let p2result = p2(&input);
+    elapsed = now.elapsed();
+
+    println!("Part 2 solution in: {:.2?}", elapsed);
+    println!("{}", p2result);
+}
+
+const INPUT_REGEX: &str = r"\[(?<lights>.+)\]\s(?<buttons>.+)\s\{(?<joltages>.+)\}"; //\s(?<buttons>((\(\d+(,\d+)*\)\s))+)\s\{(?<joltage>.+)\}";
+pub fn p1(input: &str) -> i32 {
+    let re = Regex::new(INPUT_REGEX).unwrap();
+
+    let parsed: Vec<(i16, Vec<i16>)> = input
+        .lines()
+        .map(|l| {
+            let Some(captures) = re.captures(l) else {
+                panic!("input parsing failed");
+            };
+
+            let lights = &captures["lights"];
+            let buttons = &captures["buttons"];
+            let _ = &captures["joltages"];
+            let light_len = lights.len() as u32;
+            (
+                lights
+                    .chars()
+                    .rev()
+                    .enumerate()
+                    .map(|(i, c)| match c {
+                        '#' => 2_i16.pow(i.try_into().unwrap()),
+                        _ => 0,
+                    })
+                    .sum(),
+                buttons
+                    .split(" ")
+                    .map(|b| {
+                        let mut chars = b.chars();
+                        // drop the ()
+                        chars.next();
+                        chars.next_back();
+                        chars
+                            .map(|c| match c {
+                                ',' => 0,
+                                _ => 2_i16.pow(light_len - 1 - c.to_digit(10).unwrap()),
+                            })
+                            .sum()
+                    })
+                    .collect(),
+            )
+        })
+        .collect();
+
+
+    let mut result = 0;
+    for (lights, buttons) in parsed {
+        result += find_combination(lights, buttons);
+    }
+
+    return result;
+}
+
+fn find_combination(lights: i16, buttons: Vec<i16>) -> i32 {
+    let mut queue: VecDeque<(i32, i16)> = VecDeque::new();
+    let mut computed: HashSet<i16> = HashSet::new();
+    computed.insert(0);
+    queue.push_back((0, 0));
+    while !queue.is_empty() {
+        let (count, current) = queue.pop_front().unwrap();
+        for b in buttons.iter() {
+            let value = current ^ b;
+            if !computed.contains(&value) {
+                computed.insert(value);
+                queue.push_back((count + 1, value));
+                if value == lights {
+                    return count + 1;
+                }
+            }
+        }
+    }
+    panic!("mrow");
+}
+
+pub fn p2(input: &str) -> i32 {
+    return 0;
+}
+
+
+
+// [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+
+// .....
+// #...#
+// .##.#
+// ...#.
+
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let expected = 7;
+        let example = r"[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
+";
+        assert_eq!(p1(example), expected);
+    }
+
+    #[test]
+    fn test_part2() {
+        let expected = 0;
+        let example = r"";
+        assert_eq!(p2(example), expected);
+    }
+}
