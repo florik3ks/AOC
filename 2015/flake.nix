411b170e0f9feacd1c100fb461b7a56d2af74ef3
flori <floritrs@gmail.com>
2025-12-16 15:00:27 +0100

    initialize 2015

---
+++ 2015/flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell rec {
+          packages = [
+            (pkgs.rust-bin.stable.latest.default.override {
+                extensions = [
+                  "rust-src" # for rust-analyzer
+                  "rust-analyzer"
+                  "clippy"
+                ];
+              }
+            )
+          ];
+        };
+      }
+    );
+}
